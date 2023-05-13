78999e52946485f98221770a5ea805026a21ac6d
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-05-13 22:04:20 +0900

    refactor: remove of implementations in CandyMachine component

--- components/CandyMachine/index.tsx
+++ components/CandyMachine/index.tsx
@@ -21,7 +21,6 @@ import type { Umi as UmiType } from '@metaplex-foundation/umi';
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
 import { nftStorageUploader } from '@metaplex-foundation/umi-uploader-nft-storage';
-import { useState } from 'react';
 
 import candyMachineStyles from './CandyMachine.module.css';
 
@@ -33,42 +32,6 @@ type CandyMachineProps = {
 
 const CandyMachine = (props: CandyMachineProps) => {
   const { walletAddress } = props;
-  const [umi, setUmi] = useState<UmiType | undefined>(undefined);
-  const [candyMachine, setCandyMachine] = useState<
-    CandyMachineType | undefined
-  >(undefined);
-  const [candyGuard, setCandyGuard] = useState<CandyGuardType | null>(null);
-  const [isMinting, setIsMinting] = useState(false);
-
-  const mintToken = async () => {
-    setIsMinting(true);
-    try {
-      if (umi === undefined) {
-        throw new Error('Umi context was not initialized.');
-      }
-      if (candyMachine === undefined) {
-        throw new Error('Candy Machine was not initialized.');
-      }
-      const nftSigner = generateSigner(umi);
-      // トランザクションの構築と送信を行います。
-      await transactionBuilder()
-        .add(setComputeUnitLimit(umi, { units: 600_000 }))
-        .add(
-          mintV2(umi, {
-            candyMachine: candyMachine.publicKey,
-            candyGuard: candyGuard?.publicKey,
-            nftMint: nftSigner,
-            collectionMint: candyMachine.collectionMint,
-            collectionUpdateAuthority: candyMachine.authority,
-          }),
-        )
-        .sendAndConfirm(umi);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsMinting(false);
-    }
-  };
 
   return (
     <div className={candyMachineStyles.machineContainer}>
@@ -76,8 +39,6 @@ const CandyMachine = (props: CandyMachineProps) => {
       <p>Items Minted:</p>
       <button
         className={`${styles.ctaButton} ${styles.mintButton}`}
-        onClick={mintToken}
-        disabled={isMinting}
       >
         Mint NFT
       </button>
